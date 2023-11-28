abd9cf9fd7769c914230709806c61223b74c0620
sfsf700 <hirokazu.2rnk@gmail.com>
2023-11-28 22:28:01 +0900

    Service、Repository、Daoの作成

---
+++ src/main/java/com/jp/study/service/StudyService.java
@@ -0,0 +1,27 @@
+package com.jp.study.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jp.study.entity.CustomerEntity;
+import com.jp.study.repository.StudyRepository;
+
+import io.micrometer.common.util.StringUtils;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StudyService {
+
+	private final StudyRepository studyRepository;
+	
+	public List<CustomerEntity> getCustomers(String customerCd){
+		
+		if(StringUtils.isNotEmpty(customerCd)) {
+			List<CustomerEntity> list = studyRepository.getCustomer(customerCd);
+		}
+		
+		return null;
+	}
+}
