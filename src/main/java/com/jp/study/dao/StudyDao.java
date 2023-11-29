f272793588540b851d6b8fbf32b1612aeaf60257
sfsf700 <hirokazu.2rnk@gmail.com>
2023-11-29 11:21:22 +0900

    test7

--- src/main/java/com/jp/study/dao/StudyDao.java
+++ src/main/java/com/jp/study/dao/StudyDao.java
@@ -14,4 +14,6 @@ public interface StudyDao {
 	List<CustomerEntity> selectCustomer(@Param("customerCd") String customerCd);
 	
 	int deleteByPrimary(@Param("customerCd") String customerCd);
+	
+	int updateByPrimary(@Param("customerCd") String customerCd);
 }
